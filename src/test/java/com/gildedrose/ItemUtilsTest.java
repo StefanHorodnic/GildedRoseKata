ff043cb239f6e3739297b42a91f5fe8a0c26b601
Ștefan Horodnic <stefanhorodnic@yahoo.com>
2024-08-18 12:05:42 +0300

    Added more ItemUtils tests

--- src/test/java/com/gildedrose/ItemUtilsTest.java
+++ src/test/java/com/gildedrose/ItemUtilsTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ItemUtilsTest {
 
     @Test
-    void GivenItemOfTypeAgedBrie_WhenIsBrieIsCalled_ReturnsTrue() {
+    void GivenItemOfTypeAgedBrie_WhenIsAgedBrieIsCalled_ReturnsTrue() {
         Item item = new Item("Foo Aged Brie", 2, 3);
         Item item2 = new Item("aged Brie Foo", 2, 3);
         Item item3 = new Item("aged brie", 2, 3);
@@ -43,6 +43,43 @@ class ItemUtilsTest {
             () -> assertTrue(isLegendary(item)),
             () -> assertTrue(isLegendary(item2)),
             () -> assertTrue(isLegendary(item3))
+        );
+    }
+
+    @Test
+    void GivenItemNotOfTypeAgedBrie_WhenIsNotAgedBrieIsCalled_ReturnsTrue() {
+        Item item = new Item("Foo Brie", 2, 3);
+        Item item2 = new Item("aged Foo", 2, 3);
+        Item item3 = new Item("foo", 2, 3);
+
+        assertAll(
+            () -> assertTrue(isNotAgedBrie(item)),
+            () -> assertTrue(isNotAgedBrie(item2)),
+            () -> assertTrue(isNotAgedBrie(item3))
+        );
+    }
+
+    @Test
+    void GivenItemNotOfTypeBackstagePasses_WhenIsNotBackstagePassIsCalled_ReturnsTrue() {
+        Item item = new Item("Foo passes", 2, 3);
+        Item item2 = new Item("Foo", 2, 3);
+        Item item3 = new Item("Backstage", 2, 3);
+
+        assertAll(
+            () -> assertTrue(isNotBackstagePasses(item)),
+            () -> assertTrue(isNotBackstagePasses(item2)),
+            () -> assertTrue(isNotBackstagePasses(item3))
+        );
+    }
+
+    @Test
+    void GivenItemNotOfTypeLegendary_WhenIsNotLegendaryIsCalled_ReturnsTrue() {
+        Item item = new Item("fooo", 2, 3);
+        Item item2 = new Item("Foo furas", 2, 3);
+
+        assertAll(
+            () -> assertTrue(isNotLegendary(item)),
+            () -> assertTrue(isNotLegendary(item2))
         );
     }
 }
